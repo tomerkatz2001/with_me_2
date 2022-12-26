afe0f9f11dbb87ba0e92383e14a3b5d4ae78aba7
tomerkatz <tomerkatz@campus.technion.ac.il>
2022-12-26 16:50:43 +0200

    missing file

---
+++ happy_heart/lib/Screens/deliveries.dart
@@ -0,0 +1,39 @@
+import 'package:happy_heart/header.dart';
+
+class DeliveriesPage extends StatelessWidget {
+  const DeliveriesPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: DeliverySubPages.values.length,
+      initialIndex: DeliverySubPages.values.length - 1,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('שינועים'),
+          bottom: const TabBar(
+            labelColor: Colors.black,
+            indicatorColor: Colors.red,
+            tabs: [
+              Tab(text: 'הסתיימו'),
+              Tab(text: 'בשינוע'),
+              Tab(text: "בקשות שינוע"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            DeliverySubPage(DeliverySubPages.delivered, null),
+            DeliverySubPage(DeliverySubPages.onDeliver, null),
+            DeliverySubPage(DeliverySubPages.toBeDelivered, null),
+          ],
+        ),
+        floatingActionButton: FloatingButton(() => DB.insertDelivery(Delivery(
+            Location("קפלן 42, פתח תקווה", 5, 6),
+            Location("המכבים 29, הוד השרון", 7, 8),
+            "productId",
+            "אלונקה"))),
+      ),
+    );
+  }
+}

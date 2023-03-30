3b17a9bc8684921d287fc590e1c186d4e540f4bb
idomagner <ido@assacnetworks.com>
2023-03-30 20:21:58 +0300

    add scrollability

--- with_me/lib/Screens/visit_course.dart
+++ with_me/lib/Screens/visit_course.dart
@@ -1,4 +1,3 @@
-
 import '../header.dart';
 
 class VisitCoursePage extends StatefulWidget {
@@ -8,16 +7,13 @@ class VisitCoursePage extends StatefulWidget {
 }
 
 class _VisitCoursePageState extends State<VisitCoursePage> {
-
   @override
   void initState() {
     super.initState();
   }
 
-
-  Widget visitsCourse(List<Station> stations,int current_index){
-
-    var childs =  [
+  Widget visitsCourse(List<Station> stations, int current_index) {
+    var childs = [
       SizedBox(
         height: 30,
         child: VerticalDivider(
@@ -30,17 +26,18 @@ class _VisitCoursePageState extends State<VisitCoursePage> {
         height: 30,
         decoration: BoxDecoration(
           shape: BoxShape.circle,
-          color: current_index==0?Colors.red:Colors.amberAccent,
+          color: current_index == 0 ? Colors.red : Colors.amberAccent,
         ),
       )
     ];
     List<Widget> texts = [];
-    texts.add(Column(children:[Text(stations[0].name),Text(stations[0].time)]));
-    int c=0;
+    texts.add(
+        Column(children: [Text(stations[0].name), Text(stations[0].time)]));
+    int c = 0;
     stations.forEach((station) {
-      if(c!=0) {
-        texts.add(Container(height:90));
-        texts.add(Column(children:[Text(station.name),Text(station.time)]));
+      if (c != 0) {
+        texts.add(Container(height: 90));
+        texts.add(Column(children: [Text(station.name), Text(station.time)]));
         childs.add(SizedBox(
           height: 100,
           child: VerticalDivider(
@@ -58,7 +55,6 @@ class _VisitCoursePageState extends State<VisitCoursePage> {
         ));
       }
       c += 1;
-
     });
     childs.add(SizedBox(
       height: 30,
@@ -67,34 +63,43 @@ class _VisitCoursePageState extends State<VisitCoursePage> {
         color: Colors.grey,
       ),
     ));
-    return
-      Row(children: [
-        Column(
-          children: texts,
-        ),
-        Container(width: 10),
-        Column(
-      children: childs,
-    ),
-
-      ])
-    ;
+    return Row(children: [
+      Column(
+        children: texts,
+      ),
+      Container(width: 10),
+      Column(
+        children: childs,
+      ),
+    ]);
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CircularAppBar("הביקור שלי", [
-        Container(child:Positioned(child: SingleChildScrollView(child:
-        visitsCourse([
-          Station("בדיקת דם", "באר שבע", "5:00"),
-          Station("MRI", "באר שבע", "5:00"),
-          Station("בדיקת ראש", "באר שבע", "5:00"),
-          Station("בדיקת דם", "באר שבע", "5:00"),
-          Station("MRI", "באר שבע", "5:00"),
-          Station("בדיקת ראש", "באר שבע", "5:00"),
-        ],0)), right: 20,top:200)
-        )
-      ], context)
-    );
+        body: CircularAppBar(
+            "הביקור שלי",
+            [
+              Positioned(
+                  child: Container(
+                      width: 300,
+                      height: 700,
+                      child: SingleChildScrollView(
+                          child: Positioned(
+                              child: Container(
+                                  child: visitsCourse([
+                                Station("בדיקת דם", "באר שבע", "5:00"),
+                                Station("MRI", "באר שבע", "5:00"),
+                                Station("בדיקת ראש", "באר שבע", "5:00"),
+                                Station("בדיקת דם", "באר שבע", "5:00"),
+                                Station("MRI", "באר שבע", "5:00"),
+                                Station("בדיקת ראש", "באר שבע", "5:00"),
+                              ], 0)),
+                              right: 20,
+                              top: 200))),
+                  top: 150,
+                  right: -150)
+            ],
+            context));
   }
 }
